536f08923d203fa7b77a255490e45077fd2f2b7e
Swayam Gupta <swayamgupta999@gmail.com>
2025-09-05 17:55:49 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { AdminPage } from './pages/AdminPage'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </Router>
+      <Analytics />
+      <SpeedInsights />
+    </>
   )
 }
 
